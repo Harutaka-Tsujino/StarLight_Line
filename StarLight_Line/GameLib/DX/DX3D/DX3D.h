4a4215df62e218cd12d16c7d877c8dbaecae7db9
Harutaka-Tsujino <glaca986@outlook.jp>
2018-11-20 12:06:47 +0900

    関数の戻り値確認用変数の削除

--- StarLight_Line/GameLib/DX/DX3D/DX3D.h
+++ StarLight_Line/GameLib/DX/DX3D/DX3D.h
@@ -61,9 +61,8 @@ public:
 	*/
 	inline VOID CleanUpRendering() const
 	{
-		HRESULT hr;
-		hr = m_pDX3DDev->EndScene();
-		hr = m_pDX3DDev->Present(
+		m_pDX3DDev->EndScene();
+		m_pDX3DDev->Present(
 					NULL,
 					NULL,
 					NULL,
