73834ef0514a884dca8b0fbb9bb8732522d1205f
Harutaka-Tsujino <glaca986@outlook.jp>
2018-11-22 20:30:30 +0900

    ステージセレクト時の背景クラス作成

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/StageSelectSceneObj/StageSelectSceneObj.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/StageSelectSceneObj/StageSelectSceneObj.h
@@ -1,1 +1,47 @@
-﻿#pragma once
+﻿#ifndef STAGE_SELECT_SCENE_OBJ_H
+#define STAGE_SELECT_SCENE_OBJ_H
+
+#include <windows.h>
+
+#include "../Obj.h"
+
+class StageSelectBack :public Obj
+{
+public:
+	StageSelectBack() :Obj(OT_BACK, 1.0f)
+	{
+		Init();
+	}
+
+	~StageSelectBack()
+	{
+		m_rGameLib.ReleaseTex();
+	}
+
+	inline VOID Init() const
+	{
+		static BOOL isInitialized = FALSE;
+		if (isInitialized) return;
+		isInitialized = TRUE;
+
+		m_rGameLib.CreateTex(_T("Back"), _T("2DTextures/Title/title_background.png"));
+	}
+
+	inline VOID Update() {};
+
+	inline VOID Render()
+	{
+		m_rGameLib.SetCameraTransform();
+
+		ObjData data;
+		data.m_center		= { m_WND_SIZE.m_x * 0.5f, m_WND_SIZE.m_y * 0.5f, 1.0f };
+		data.m_halfScale	= { m_WND_SIZE.m_x * 0.5f, m_WND_SIZE.m_y * 0.5f, 0.0f };
+
+		CustomVertex back[4];
+		m_rGameLib.CreateRect(back, data);
+
+		m_rGameLib.Render(back, m_rGameLib.GetTex(_T("Back")));
+	}
+};
+
+#endif // !STAGE_SELECT_SCENE_OBJ_H
