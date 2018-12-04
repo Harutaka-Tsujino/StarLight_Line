5b32c1372223091be6658beb8b0dff0b1063410c
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2018-12-04 23:59:31 +0900

    ファイルの追加
    ファイルの追加

---
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerHP/PlayerHP.h
@@ -0,0 +1,28 @@
+﻿#ifndef PLAYER_HP_H_
+#define PLAYER_HP_H_
+
+#include "../../../../../../../../../GameLib/GameLib.h"
+#include "../../../Obj.h"
+
+class PlayerHP:public Obj
+{
+public:
+	PlayerHP() :Obj(OT_UI, 0.0f)
+	{
+		Init();
+	}
+
+	~PlayerHP()
+	{
+		
+	}
+
+	VOID Init();
+	VOID Update();
+	VOID Render();
+
+private:
+	INT m_HP;
+};
+
+#endif // !PLAYER_HP_H_
