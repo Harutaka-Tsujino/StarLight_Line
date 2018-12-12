4ce5a6de4fc1ba78ad738ab25a1390d1b1da7e7c
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2018-12-12 14:09:50 +0900

    プレイヤーのHPを減らす処理の追加

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerHP/PlayerHP.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerHP/PlayerHP.h
@@ -1,28 +1,43 @@
 ﻿#ifndef PLAYER_HP_H_
 #define PLAYER_HP_H_
 
-#include "../../../../../../../../../GameLib/GameLib.h"
-#include "../../../Obj.h"
+#include <vector>
 
-class PlayerHP:public Obj
+#include "../../../Obj.h"
+#include "../../../../../../../../../GameLib/GameLib.h"
+#include "../../GameCollision/GameCollision.h"
+#include "../../Enum/STAR_TYPE.h"
+
+class PlayerHP :public Obj
 {
 public:
-	PlayerHP() :Obj(OT_UI, 0.0f)
+	PlayerHP() :Obj(OT_UI, 0.0f), m_rGameCollision(GameCollision::GetInstance())
 	{
 		Init();
 	}
 
 	~PlayerHP()
 	{
-		
+		ReleaseVertex();
+		m_rGameLib.ReleaseTex();
 	}
 
 	VOID Init();
 	VOID Update();
 	VOID Render();
 
+	VOID CreateVertex();
+	VOID ReleaseVertex();
+	INT GetHP() { return m_HP; }
+
 private:
+	VOID CutUV(TexUV* UV);
+
+	std::vector<CustomVertex*> m_HpRect;
+
 	INT m_HP;
+
+	GameCollision& m_rGameCollision;
 };
 
 #endif // !PLAYER_HP_H_
