6a0adf563b771c4cbde86b8b9433917b4c6ac54e
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2018-12-14 13:15:18 +0900

    マージ

---
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerEffect/PlayerEffect.cpp
@@ -0,0 +1,18 @@
+﻿#include "PlayerEffect.h"
+
+#include "../../../../../../../../../GameLib/Wnd/Wnd.h"
+
+VOID PlayerEffect::Init()
+{
+
+}
+
+VOID PlayerEffect::Update()
+{
+
+}
+
+VOID PlayerEffect::Render()
+{
+
+}
---
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerEffect/PlayerEffect.h
@@ -0,0 +1,28 @@
+﻿#ifndef PLAYER_EFFECT_H_
+#define PLAYER_EFFECT_H_
+
+#include <d3dx9.h>
+
+#include "../../../Obj.h"
+
+class PlayerEffect :public Obj
+{
+public:
+	PlayerEffect() :Obj(OT_TRANSPARENCY, 0.0f)
+	{
+		Init();
+	}
+
+	~PlayerEffect()
+	{
+
+	}
+
+	VOID Init();
+	VOID Update();
+	VOID Render();
+
+};
+
+#endif // !
+
