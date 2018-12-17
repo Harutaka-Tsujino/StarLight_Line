a02f1559de72bbdb64cf8a4acbbdb65950e158cb
Harutaka-Tsujino <glaca986@outlook.jp>
2018-12-17 09:51:46 +0900

    構造体のヘッダのフォルダの名前がStructになっているものがあったのでDataに統一

---
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/ADV/Chapter/Section/Text/Data/TextFormat.h
@@ -0,0 +1,24 @@
+﻿/// <filename>
+/// 
+/// </filename>
+/// <summary>
+///
+/// </summary>
+/// <author>
+/// 
+/// </author>
+
+#ifndef TEXT_FORMAT_H
+#define TEXT_FORMAT_H
+
+#include <d3dx9.h>
+
+#include "../../../../../../../../../../../../GameLib/Wnd/Data/RectSize.h"
+
+struct TextFormat
+{
+public:
+
+};
+
+#endif //! TEXT_FORMAT_H
