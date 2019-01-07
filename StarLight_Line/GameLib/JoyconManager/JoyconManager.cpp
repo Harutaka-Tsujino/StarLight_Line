d004c080f527906eb5108a739e117e25c62212cd
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-07 13:28:07 +0900

    ファイルの追加
    ファイルの追加

---
+++ StarLight_Line/GameLib/JoyconManager/JoyconManager.cpp
@@ -0,0 +1,53 @@
+﻿#include "JoyconManager.h"
+
+//----------------------------------------------------------------------
+// Constructor	Destructor
+//----------------------------------------------------------------------
+
+JoyconManager::JoyconManager()
+{
+	for (int i = 0; i < Joycon::MAX_CONTROLLER; i++)
+	{
+		m_pJoycon[i] = new Joycon();
+	}
+}
+
+JoyconManager::~JoyconManager()
+{
+	for (int i = 0; i < Joycon::MAX_CONTROLLER; i++)
+	{
+		delete m_pJoycon[i];
+		m_pJoycon[i] = nullptr;
+	}
+	hid_exit();
+}
+
+bool JoyconManager::SearchAnalogStick(Joycon::CONTROLLER_TYPE controllerType, int direction)
+{
+	const FLOAT THRESHOLD_VALUE = 0.15f;
+
+	switch (direction)
+	{
+	case Joycon::UP_SLOPE:
+		if (m_pJoycon[controllerType]->GetAnalogStick().y > THRESHOLD_VALUE) return TRUE;
+
+		break;
+
+	case Joycon::DOWN_SLOPE:
+		if (m_pJoycon[controllerType]->GetAnalogStick().y < -THRESHOLD_VALUE) return TRUE;
+
+		break;
+
+	case Joycon::RIGHT_SLOPE:
+		if (m_pJoycon[controllerType]->GetAnalogStick().x < -THRESHOLD_VALUE) return TRUE;
+
+		break;
+
+	case Joycon::LEFT_SLOPE:
+		if (m_pJoycon[controllerType]->GetAnalogStick().x > THRESHOLD_VALUE) return TRUE;
+
+		break;
+	}
+
+	return FALSE;
+}
---
+++ StarLight_Line/GameLib/JoyconManager/JoyconManager.h
@@ -0,0 +1,54 @@
+﻿#ifndef JOYCON_MANAGER_H_
+#define JOYCON_MANAGER_H_
+
+#include "Joycon/Joycon.h"
+
+class JoyconManager
+{
+public:
+	JoyconManager();
+
+	~JoyconManager();
+
+	/**
+	* Joyconを取得する
+	* @param controllerType 取得するコントローラの種類
+	*/
+	Joycon* GetJoycon(Joycon::CONTROLLER_TYPE controllerType) const
+	{
+		return m_pJoycon[controllerType];
+	}
+
+	bool Connect(Joycon::CONTROLLER_TYPE controllerType) const
+	{
+		return m_pJoycon[controllerType]->Connect(controllerType);
+	}
+
+	void Disconnect(Joycon::CONTROLLER_TYPE controllerType) const
+	{
+		m_pJoycon[controllerType]->Disconnect();
+	}
+
+	void CheckButton(Joycon::CONTROLLER_TYPE controllerType, int button) const
+	{
+		m_pJoycon[controllerType]->CheckButton(button);
+	}
+
+	/**
+	* 引数に渡した向きを入れると
+	* アナログスティックがその方向に向いているかを判断する関数
+	*
+	* @param controllerType 取得するコントローラの種類
+	* @param Direction アナログスティックの向き
+	*/
+	bool SearchAnalogStick(Joycon::CONTROLLER_TYPE controllerType, int direction);
+
+private:
+	JoyconManager(const JoyconManager&) = delete;
+	JoyconManager& operator=(const JoyconManager&) = delete;
+
+	Joycon* m_pJoycon[Joycon::MAX_CONTROLLER];
+
+};
+
+#endif
