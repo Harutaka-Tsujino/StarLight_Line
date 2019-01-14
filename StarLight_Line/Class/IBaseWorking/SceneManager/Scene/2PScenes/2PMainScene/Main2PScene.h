58698598dc4dc27e4260f1e6c3a67e70885eeb97
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-14 22:25:58 +0900

    ファイルの追加
    ファイルの追加」
    
    2PMainプレイのシーンクラスの追加
    
    2Pメインオブジェクトのファイルの追加

---
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PMainScene/Main2PScene.h
@@ -0,0 +1,12 @@
+﻿#ifndef MAIN_TWO_PLAYER_SCENE_H_
+#define MAIN_TWO_PLAYER_SCENE_H_
+
+#include "../../Scene.h"
+
+class MainTwoPlayerScene :public Scene
+{
+public:
+
+};
+
+#endif // !MAIN_TWO_PLAYER_SCENE_H_
