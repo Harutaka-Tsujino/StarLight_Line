9dc363801fc68182fe77bcb05926b2ffbc05aa94
Harutaka-Tsujino <glaca986@outlook.jp>
2019-01-15 10:30:56 +0900

    Effect関係クラスの作成

--- StarLight_Line/GameLib/EffectManager/Effect/Effect.cpp
+++ StarLight_Line/GameLib/EffectManager/Effect/Effect.cpp
@@ -1,1 +1,32 @@
-﻿
+﻿#include "Effect.h"
+
+#include <windows.h>
+
+#include <d3dx9.h>
+
+#include <vector>
+
+#include "Particle/Particle.h"
+
+VOID Effect::CountUpActiveLimit()
+{
+	if (m_COUNT_TO_ACTIVE_MAX == 0)
+	{
+		m_activeLimit = static_cast<INT>(m_particles.size());
+
+		return;
+	}
+
+	++m_countToActive;
+
+	if (m_countToActive >= m_COUNT_TO_ACTIVE_MAX - 1)
+	{
+		m_countToActive = 0;
+
+		++m_activeLimit;
+	}
+
+	INT particleSize = static_cast<INT>(m_particles.size());
+
+	if (m_activeLimit >= particleSize) m_activeLimit = particleSize;
+}
--- StarLight_Line/GameLib/EffectManager/Effect/Effect.h
+++ StarLight_Line/GameLib/EffectManager/Effect/Effect.h
@@ -5,15 +5,77 @@
 
 #include <d3dx9.h>
 
+#include <vector>
+
+#include "Particle/Particle.h"
+
 class Effect
 {
 public:
-	Effect() {};
-	~Effect() {};
-
-	VOID Render()
+	Effect(size_t particlesNum, const TCHAR* pTexPath, INT activeCount) :m_COUNT_TO_ACTIVE_MAX(activeCount)
 	{
+		m_particles.resize(particlesNum);
+		for (auto& i : m_particles) i = new Particle(pTexPath);
 	}
+
+	virtual ~Effect() 
+	{
+		for (auto i : m_particles) delete i;
+		
+		m_particles.clear();
+		m_particles.shrink_to_fit();
+	}
+
+	virtual inline VOID Update() = 0;
+
+	inline VOID Render()
+	{
+		m_rGameLib.AddtionBlendMode();
+
+		for (INT i = 0; i < m_particles.size(); ++i)
+		{
+			if (i > m_activeLimit) continue;
+
+			m_particles[i]->Render();
+		}
+
+		m_rGameLib.DefaultBlendMode();
+
+		CountUpActiveLimit();
+	}
+
+	inline BOOL Ends() const
+	{
+		return m_ends;
+	}
+
+protected:
+	VOID CountUpActiveLimit();
+
+	inline VOID InitActivatedParticle()
+	{
+		for (INT i = 0; i < m_particles.size(); ++i)
+		{
+			if (i == m_activeLimit && m_countToActive == 0 && m_COUNT_TO_ACTIVE_MAX != 0)
+			{
+				Init(m_particles[i]);
+
+				break;
+			}
+		}
+	}
+
+	virtual VOID Init(Particle* pParticle) {};
+
+	GameLib& m_rGameLib = GameLib::GetInstance();
+
+	std::vector<Particle*> m_particles;
+
+	const INT m_COUNT_TO_ACTIVE_MAX = 0;
+	INT m_countToActive = 0;
+	INT m_activeLimit = 0;
+
+	BOOL m_ends = FALSE;
 };
 
 #endif //! EFFECT_H
