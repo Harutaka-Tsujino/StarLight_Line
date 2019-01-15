193c797ea0f760b8bc00200901e1e96309d5e1dd
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-15 11:21:32 +0900

    2p時の星の挙動を追加

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PMainScene/Main2PScene.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PMainScene/Main2PScene.h
@@ -2,11 +2,22 @@
 #define MAIN_TWO_PLAYER_SCENE_H_
 
 #include "../../Scene.h"
+#include "../../ObjManager/Obj/MainSceneObj/MainSceneObject.h"
+#include "../../ObjManager/Obj/MainSceneObj/StarManager/StarManager.h"
 
 class MainTwoPlayerScene :public Scene
 {
 public:
+	MainTwoPlayerScene()
+	{
+		m_pObjManager->Resister(static_cast<Obj*>(new MainBack()));
+		m_pObjManager->Resister(static_cast<Obj*>(new MainLine()));
+		m_pObjManager->Resister(static_cast<Obj*>(new StarManager()));
+	}
 
+	~MainTwoPlayerScene()
+	{
+	}
 };
 
 #endif // !MAIN_TWO_PLAYER_SCENE_H_
