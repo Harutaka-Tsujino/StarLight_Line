a6052cacc8a969979f25383c8c93a22059fa805c
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-16 10:46:36 +0900

    ２Pメインシーンにオブジェクトクラスの追加

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PMainScene/Main2PScene.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PMainScene/Main2PScene.h
@@ -4,14 +4,23 @@
 #include "../../Scene.h"
 #include "../../ObjManager/Obj/MainSceneObj/MainSceneObject.h"
 #include "../../ObjManager/Obj/MainSceneObj/StarManager/StarManager.h"
+#include "../../ObjManager/Obj/2PSceneObjects/Main2PObj/Main2PObj.h"
+#include "../../ObjManager/Obj/2PSceneObjects/Main2PObj/Player1/Player1.h"
 
 class MainTwoPlayerScene :public Scene
 {
 public:
 	MainTwoPlayerScene()
 	{
+		GameLib& rGameLib = GameLib::GetInstance();
+
+		rGameLib.SetCameraPos(0.0f, -2.15f, -1.0f);
+		rGameLib.SetCameraTransform();
+
 		m_pObjManager->Resister(static_cast<Obj*>(new MainBack()));
 		m_pObjManager->Resister(static_cast<Obj*>(new MainLine()));
+		m_pObjManager->Resister(static_cast<Obj*>(new ScoreFrameforTwoPlayer()));
+		m_pObjManager->Resister(static_cast<Obj*>(new PlayerOne()));
 		m_pObjManager->Resister(static_cast<Obj*>(new StarManager()));
 	}
 
