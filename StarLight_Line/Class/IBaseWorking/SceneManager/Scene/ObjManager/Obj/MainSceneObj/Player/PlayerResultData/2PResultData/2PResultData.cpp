14f8007bd01909a5e3333460f808d3e64545c2db
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-16 17:31:12 +0900

    2Pのリザルトデータクラスの追加

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerResultData/2PResultData/2PResultData.cpp
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerResultData/2PResultData/2PResultData.cpp
@@ -1,8 +1,10 @@
 ﻿#include "2PResultData.h"
 
+#include <math.h>
+
 #include "../../../ADV/Chapter/Section/Text/Tstring/TString.h"
 #include "../../../ADV/Chapter/Section/Text/Data/TextFormat.h"
-#include "../../../ADV/Chapter/Section/Text/NumText/NumText.h"
+#include "../../../ADV/Chapter/Section/Text/Text.h"
 
 VOID TwoPlayerResultData::Init()
 {
@@ -16,10 +18,10 @@ VOID TwoPlayerResultData::Init()
 
 VOID TwoPlayerResultData::Update()
 {
-	const FLOAT PLAYER_RADIUS = 20.f;
-	const FLOAT ENEMY_RADIUS  = 30.f;
+	//当たり判定の取り方
+	const INT HIT_MODE = 2;
 
-	if (m_rGameCollision.HitSomething(m_PlayerTypeKey, DAMAGE, PLAYER_RADIUS, ENEMY_RADIUS))
+	if (m_rGameCollision.HitSomething(m_PlayerTypeKey, DAMAGE, HIT_MODE))
 	{
 		m_rGameLib.OneShotSimultaneousSound(_T("HitWhite"));
 
@@ -28,7 +30,7 @@ VOID TwoPlayerResultData::Update()
 		m_Score += DEMERIT_POINT;
 	}
 
-	if (m_rGameCollision.HitSomething(m_PlayerTypeKey, SCORE, PLAYER_RADIUS, ENEMY_RADIUS))
+	if (m_rGameCollision.HitSomething(m_PlayerTypeKey, SCORE, HIT_MODE))
 	{
 		m_rGameLib.OneShotSimultaneousSound(_T("HitBlue"));
 
@@ -37,7 +39,7 @@ VOID TwoPlayerResultData::Update()
 		m_Score += BASIC_POINT;
 	}
 
-	if (m_rGameCollision.HitSomething(m_PlayerTypeKey, CLEAR, PLAYER_RADIUS, ENEMY_RADIUS))
+	if (m_rGameCollision.HitSomething(m_PlayerTypeKey, CLEAR, HIT_MODE))
 	{
 		m_rGameLib.OneShotSimultaneousSound(_T("HitGreen"));
 
@@ -46,18 +48,29 @@ VOID TwoPlayerResultData::Update()
 		m_Score += BONUS_POINT;
 	}
 
+	if (m_controllerType == Joycon::RIGHT_CONTROLLER)
+	{
+		m_rGameCollision.DisableEnemyHitForHit();
+	}
+
 	//マイナスに行かないようにする
 	m_Score = (m_Score < 0) ? 0 : m_Score;
 }
 
 VOID TwoPlayerResultData::Render()
 {
-	TextFormat txtFormat;
-	txtFormat.m_charHalfScale = { 14, 14 };
-	txtFormat.m_charGap = { -10.0f, 0.0f };
-	txtFormat.m_topLeft = { 150, 175 + 215.0f };
+	INT ScoreDigit = m_Score;
 
-	TString ScoreNum(m_Score, _T("%2d"));
-	NumText ScoreText(ScoreNum, _T("2DTextures/Result/Nums.png"));
+	ScoreDigit = static_cast<int>(log10(ScoreDigit) + 1);
+
+	if (m_Score == 0) ScoreDigit = 1;
+
+	TextFormat txtFormat;
+	txtFormat.m_charHalfScale = { 15, 15 };
+	txtFormat.m_charGap = { 10.0f, 0.0f };
+	txtFormat.m_topLeft = { 500.f - ScoreDigit * txtFormat.m_charHalfScale.m_x * 2.0f + (630.f * m_controllerType), m_WND_SIZE.m_y * 0.88f };
+
+	TString ScoreNum(m_Score, _T("%d"));
+	Text ScoreText(ScoreNum, _T("2DTextures/Fonts/a_9.png"));
 	ScoreText.Write(txtFormat);
 }
--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerResultData/2PResultData/2PResultData.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/Player/PlayerResultData/2PResultData/2PResultData.h
@@ -11,8 +11,11 @@
 class TwoPlayerResultData :public Obj
 {
 public:
-	TwoPlayerResultData(const TCHAR* PlayerKey)
-		:Obj(OT_TRANSPARENCY, 1.0f), m_rGameCollision(GameCollision::GetInstance()), m_PlayerTypeKey(PlayerKey)
+	TwoPlayerResultData(const TCHAR* PlayerKey, Joycon::CONTROLLER_TYPE controllerType)
+		:Obj(OT_TRANSPARENCY, 1.0f),
+		m_rGameCollision(GameCollision::GetInstance()),
+		m_PlayerTypeKey(PlayerKey),
+		m_controllerType(controllerType)
 	{
 		Init();
 	}
@@ -29,7 +32,8 @@ public:
 private:
 	INT m_Score;
 	const TCHAR* m_PlayerTypeKey;	//当たり判定用
-
+	
+	Joycon::CONTROLLER_TYPE m_controllerType;
 	GameCollision& m_rGameCollision;
 };
 
