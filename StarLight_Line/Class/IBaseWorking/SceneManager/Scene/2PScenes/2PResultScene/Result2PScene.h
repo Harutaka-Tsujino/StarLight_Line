8eed0215918177b2ee2b695ad737da890d5f0c46
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-16 19:37:55 +0900

    リザルトシーンクラスの追加

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PResultScene/Result2PScene.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PResultScene/Result2PScene.h
@@ -1,1 +1,19 @@
-﻿#pragma once
+﻿#ifndef RESULT_TWO_PLAYER_SCENE_H_
+#define RESULT_TWO_PLAYER_SCENE_H_
+
+#include "../../Scene.h"
+
+class ResultTwoPlayerScene :public Scene
+{
+public:
+	ResultTwoPlayerScene()
+	{
+
+	}
+
+	~ResultTwoPlayerScene()
+	{
+	}
+};
+
+#endif // !RESULT_TWO_PLAYER_SCENE_H_
