6f0c61c7fe75b2734da15d6a5110ca92e54d2f2f
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-16 10:49:39 +0900

    ２Pプレイ時の1Pの動きを追加

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/2PSceneObjects/Main2PObj/Player1/Player1.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/2PSceneObjects/Main2PObj/Player1/Player1.h
@@ -1,1 +1,29 @@
-﻿#pragma once
+﻿#ifndef PLAYER_ONE_H_
+#define PLAYER_ONE_H_
+
+#include "../../../MainSceneObj/Player/PlayerBase/PlayerBase.h"
+
+class PlayerOne :public PlayerBase
+{
+public:
+	PlayerOne()
+	{
+		Init();
+	}
+
+	~PlayerOne()
+	{
+		m_rGameLib.GetFbx(_T("Eiwi")).Release();
+	}
+
+	VOID Init();
+	VOID Update();
+	VOID Render();
+
+private:
+	VOID SetPlayerFbxMaterial();
+
+	Joycon::CONTROLLER_TYPE m_contrllerType = Joycon::LEFT_CONTROLLER;
+};
+
+#endif // !PLAYER_ONE_H_
