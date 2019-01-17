1b06554e1181c3b589b269bb3ba18941500dcfa6
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-01-17 13:48:00 +0900

    ２P リザルトシーンの背景と文字の描画

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/2PSceneObjects/Result2PObj/Result2PObj.cpp
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/2PSceneObjects/Result2PObj/Result2PObj.cpp
@@ -1,1 +1,58 @@
-﻿
+﻿#include "Result2PObj.h"
+
+VOID ResultTwoPlayerBack::TextRender()
+{
+	TString resultString(_T("RESULT"));
+
+	Text resultText(resultString, _T("2DTextures/Fonts/a_9.png"));
+
+	TextFormat txtFormat;
+	txtFormat.m_charHalfScale = { 27, 41 };
+
+	//! RESULTの文字数の半分が3
+	txtFormat.m_topLeft = { m_WND_SIZE.m_x * 0.5f - 2.0f * txtFormat.m_charHalfScale.m_x * 3 , 13.0f };
+
+	resultText.Write(txtFormat);
+}
+
+VOID ResultTwoPlayerFrame::FrameRender()
+{
+	for (int i = 0;i < PLAYER_TYPE_MAX;++i)
+	{
+		ObjData data;
+		data.m_center = { m_WND_SIZE.m_x * (0.25f +(0.25f * (i * 2))), m_WND_SIZE.m_y * 0.75f, m_Z };
+		data.m_halfScale = { m_WND_SIZE.m_x * 0.2f, m_WND_SIZE.m_y * 0.5f, 0.0f };
+
+		data.m_aRGB = D3DCOLOR_ARGB(80, 255, 255, 255);
+
+		CustomVertex back[4];
+		m_rGameLib.CreateRect(back, data);
+
+		m_rGameLib.Render(back, nullptr);
+	}
+}
+
+VOID ResultTwoPlayerFrame::PlayerTextRender()
+{
+	TString OnePlayerString(_T("1P"));
+
+	Text OnePlayerText(OnePlayerString, _T("2DTextures/Fonts/a_9.png"));
+
+	TextFormat txt1pFormat;
+	txt1pFormat.m_charHalfScale = { 30, 35 };
+
+	txt1pFormat.m_topLeft = { m_WND_SIZE.m_x * 0.25f - txt1pFormat.m_charHalfScale.m_x * 2.0f, 200.0f };
+
+	OnePlayerText.Write(txt1pFormat);
+
+	TString TwoPlayerString(_T("2P"));
+
+	Text TwoPlayerText(TwoPlayerString, _T("2DTextures/Fonts/a_9.png"));
+
+	TextFormat txt2PFormat;
+	txt2PFormat.m_charHalfScale = { 30, 35 };
+
+	txt2PFormat.m_topLeft = { m_WND_SIZE.m_x * 0.75f - txt2PFormat.m_charHalfScale.m_x * 2.0f , 200.0f };
+
+	TwoPlayerText.Write(txt2PFormat);
+}
--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/2PSceneObjects/Result2PObj/Result2PObj.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/2PSceneObjects/Result2PObj/Result2PObj.h
@@ -1,1 +1,108 @@
-﻿#pragma once
+﻿#ifndef RESULT_TWO_PLAYER_OBJ_H_
+#define RESULT_TWO_PLAYER_OBJ_H_
+
+#include "../../../../../SceneManager.h"
+#include "../../Obj.h"
+#include "../../MainSceneObj/ADV/Chapter/Section/Text/Tstring/TString.h"
+#include "../../MainSceneObj/ADV/Chapter/Section/Text/Data/TextFormat.h"
+#include "../../MainSceneObj/ADV/Chapter/Section/Text/Text.h"
+
+class ResultTwoPlayerBack:public Obj
+{
+public:
+	ResultTwoPlayerBack() :Obj(OT_BACK, 1.0f)
+	{
+		Init();
+	}
+
+	~ResultTwoPlayerBack()
+	{
+		m_rGameLib.ReleaseTex();
+	}
+
+	VOID Init() const
+	{
+		m_rGameLib.CreateTex(_T("Back"), _T("2DTextures/SaveData/SaveData_Back.png"));
+	}
+
+	VOID Update() {};
+	
+	VOID Render()
+	{
+		m_rGameLib.SetCameraTransform();
+
+		TextRender();
+
+		ObjData data;
+		data.m_center = { m_WND_SIZE.m_x * 0.5f, m_WND_SIZE.m_y * 0.5f, m_Z };
+		data.m_halfScale = { m_WND_SIZE.m_x * 0.5f, m_WND_SIZE.m_y * 0.5f, 0.0f };
+
+		CustomVertex back[4];
+		m_rGameLib.CreateRect(back, data);
+
+		m_rGameLib.Render(back, m_rGameLib.GetTex(_T("Back")));
+	}
+
+private:
+	VOID TextRender();	//resultの文字を描画
+};
+
+class ResultTwoPlayerFrame :public Obj
+{
+public:
+	ResultTwoPlayerFrame() :Obj(OT_UI, 0.9)
+	{
+		Init();
+	}
+
+	~ResultTwoPlayerFrame()
+	{
+
+	}
+
+	VOID Init() {};
+	VOID Update() {};
+
+	VOID Render()
+	{
+		PlayerTextRender();
+
+		FrameRender();
+	}
+
+private:
+	const INT PLAYER_TYPE_MAX = 2;
+
+	VOID FrameRender();
+
+	VOID PlayerTextRender();
+};
+
+class ResultTwoPlayerScore :public Obj
+{
+public:
+	ResultTwoPlayerScore() :Obj(OT_UI, 0.9f)
+	{
+		Init();
+	}
+
+	~ResultTwoPlayerScore();
+
+	VOID Init()
+	{
+		SceneManager& rSceneManager = SceneManager::GetInstance();
+
+		for (int i = 0;i < Joycon::MAX_CONTROLLER;++i)
+		{
+			m_Score[i] = rSceneManager.Get2PModeScore(i);
+		}
+	}
+
+	VOID Update();
+	VOID Render();
+
+private:
+	INT m_Score[Joycon::MAX_CONTROLLER];
+};
+
+#endif // !RESULT_TWO_PLAYER_OBJ_H_
