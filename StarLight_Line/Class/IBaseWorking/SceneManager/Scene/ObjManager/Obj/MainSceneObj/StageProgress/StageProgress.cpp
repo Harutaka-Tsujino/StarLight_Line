daa951f62eb8ddc0575ad1c3a645d6c39fb791ff
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-24 23:43:28 +0900

    inline関数から普通の普通の関数に変更

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/StageProgress/StageProgress.cpp
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/StageProgress/StageProgress.cpp
@@ -1,1 +1,37 @@
-﻿
+﻿#include "StageProgress.h"
+
+VOID StageProgress::Update()
+{
+	m_pADV->Update();
+
+	if (m_pADV->IsActive())
+	{
+		return;
+	}
+
+	for (auto i : m_pObjs)
+	{
+		i->Update();
+	}
+
+	m_pStarManager->Update();
+	m_pPlayer->Update();
+}
+
+VOID StageProgress::Render()
+{
+	m_pADV->Render();
+
+	if (m_pADV->IsActive())
+	{
+		return;
+	}
+
+	for (auto i : m_pObjs)
+	{
+		i->Render();
+	}
+
+	m_pStarManager->Render();
+	m_pPlayer->Render();
+}
