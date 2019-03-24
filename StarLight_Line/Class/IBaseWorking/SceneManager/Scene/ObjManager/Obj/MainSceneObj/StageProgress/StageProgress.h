cfd756fa1081a5ba213345ce154ec0473fba89be
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-24 23:43:28 +0900

    inline関数から普通の普通の関数に変更

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/StageProgress/StageProgress.h
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/ObjManager/Obj/MainSceneObj/StageProgress/StageProgress.h
@@ -48,41 +48,9 @@ public:
 		}
 	}
 
-	inline VOID Update()
-	{
-		m_pADV->Update();
+	VOID Update();
 
-		if (m_pADV->IsActive())
-		{
-			return;
-		}
-
-		for (auto i : m_pObjs)
-		{
-			i->Update();
-		}
-
-		m_pStarManager->Update();
-		m_pPlayer->Update();
-	}
-
-	inline VOID Render()
-	{
-		m_pADV->Render();
-
-		if (m_pADV->IsActive())
-		{
-			return;
-		}
-
-		for (auto i : m_pObjs)
-		{
-			i->Render();
-		}
-
-		m_pStarManager->Render();
-		m_pPlayer->Render();
-	}
+	VOID Render();
 
 private:
 	StageProgress & operator=(const StageProgress&) = delete;
