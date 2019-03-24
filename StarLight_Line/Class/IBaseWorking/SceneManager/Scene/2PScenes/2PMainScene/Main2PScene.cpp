1feaa7b879848fbf233cc7a24329c7e0dd48eb14
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-24 23:39:09 +0900

    2PMainSceneクラスのコンストラクタをcppファイルに書き直す

--- StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PMainScene/Main2PScene.cpp
+++ StarLight_Line/Class/IBaseWorking/SceneManager/Scene/2PScenes/2PMainScene/Main2PScene.cpp
@@ -1,1 +1,16 @@
-﻿
+﻿#include "Main2PScene.h"
+
+MainTwoPlayerScene::MainTwoPlayerScene()
+{
+	GameLib& rGameLib = GameLib::GetInstance();
+
+	rGameLib.SetCameraPos(0.0f, -2.15f, -1.0f);
+	rGameLib.SetCameraTransform();
+
+	m_pObjManager->Resister(static_cast<Obj*>(new PlayerTwo()));
+	m_pObjManager->Resister(static_cast<Obj*>(new MainBack()));
+	m_pObjManager->Resister(static_cast<Obj*>(new MainLine()));
+	m_pObjManager->Resister(static_cast<Obj*>(new ScoreFrameforTwoPlayer()));
+	m_pObjManager->Resister(static_cast<Obj*>(new StarManager()));
+	m_pObjManager->Resister(static_cast<Obj*>(new PlayerOne()));
+}
